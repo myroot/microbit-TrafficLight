5a0a10f6161823e63ea784ba295d830c843bd6d7
Seungkeun Lee <sngn.lee@samsung.com>
2022-07-31 12:01:46 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,51 +1,40 @@
-basic.showIcon(IconNames.Heart)
-let color = 0
-basic.forever(function () {
-    color += 1
-    if (color % 3 == 0) {
+function turnOn (num: number) {
+    if (num < 10) {
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    } else if (num < 50) {
         pins.digitalWritePin(DigitalPin.P16, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 0)
-        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
-        basic.showIcon(IconNames.No)
-        basic.pause(10000)
-    } else if (color % 3 == 1) {
-        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    } else if (num < 80) {
+        pins.digitalWritePin(DigitalPin.P16, 1)
         pins.digitalWritePin(DigitalPin.P1, 1)
         pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(1000)
-    } else {
-        pins.digitalWritePin(DigitalPin.P16, 0)
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    } else if (num < 100) {
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
         pins.digitalWritePin(DigitalPin.P2, 1)
-        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
-        basic.showLeds(`
-            . # # . .
-            # . . # .
-            # . . . #
-            # . . . #
-            . # # # .
-            `)
-        basic.pause(5000)
-        for (let index = 0; index < 10; index++) {
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            basic.pause(500)
-            pins.digitalWritePin(DigitalPin.P2, 1)
-            basic.showLeds(`
-                . # # . .
-                # . . # .
-                # . . . .
-                # . . . #
-                . # # # .
-                `)
-            basic.pause(500)
-        }
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+    }
+}
+input.onSound(DetectedSound.Loud, function () {
+    for (let index = 0; index < 4; index++) {
+        turnOn(input.soundLevel())
+        basic.pause(100)
+        turnOn(input.soundLevel())
+        basic.pause(100)
+        turnOn(input.soundLevel())
+        basic.pause(100)
     }
 })
+basic.showIcon(IconNames.Heart)
+let color = 0
